d941f2863d06d4b6c23404fa054fedfb972191d5
Andrew <dao-yan@yandex.ru>
2020-07-09 14:15:25 +0300

    Comp task-4

--- task-4.py
+++ task-4.py
@@ -4,3 +4,19 @@
 Найдите самую большую цифру в числе.
 Для решения используйте цикл while и арифметические операции.
 """
+
+number = int(input("Введите число "))
+
+max_number = 0
+
+while number // 10:
+    next_number = number % 10
+    number = number // 10
+
+    if max_number < next_number:
+        max_number = next_number
+else:
+    if max_number < number:
+        max_number = number
+
+print("Максимальное число {}".format(max_number))
