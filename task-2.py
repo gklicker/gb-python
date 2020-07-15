8746195b675c1831c27d0bc301c887aa393cd358
Andrey Yushkov <dao-yan@live.ru>
2020-07-15 20:52:19 +0300

    Comp task-2

--- task-2.py
+++ task-2.py
@@ -5,3 +5,9 @@
 Функция должна принимать параметры как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.
 """
 
+
+def my_join(fname, lname, byear, cyear, email, phone):
+    return "{}, {}, {}, {}, {}, {}".format(fname, lname, byear, cyear, email, phone)
+
+
+print(my_join("Иван", "Иванов", 1989, 2020, "a@a.ru", "89000000000"))
