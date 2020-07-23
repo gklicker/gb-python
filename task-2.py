847b3848255f73e80ee78a8bb2aea1d42e363095
Andrew <dao-yan@yandex.ru>
2020-07-23 15:35:29 +0300

    Comp task-2

--- task-2.py
+++ task-2.py
@@ -3,3 +3,11 @@
 Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+
+line_counter = 0
+with open(file="sample.txt", mode="r") as fh:
+    for line in fh:
+        line_counter += 1
+        word_counter = len(line.split())
+        print("В строке {} всего {} слов(а)".format(line_counter, word_counter))
+print("Всего строк: {}".format(line_counter))
