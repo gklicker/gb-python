b6d872c1e88e7285efcc50d948b0e6ec3c6d40cb
Andrew <dao-yan@yandex.ru>
2020-07-23 17:39:58 +0300

    Comp task-4

--- task-4.py
+++ task-4.py
@@ -9,3 +9,12 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+
+dic = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+
+with open(file="numbers_another.txt", mode="a", encoding="utf-8") as fo0:
+    with open(file="numbers.txt", mode="r", encoding="utf-8") as fo1:
+        for line in fo1:
+            my_list = line.split()
+            print("{} {} {}".format(dic[my_list[0]], my_list[1], my_list[2]), file=fo0)
