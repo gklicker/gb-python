d85a789447b3c4724478de58b4dd2b4d2bf5da01
Andrey Yushkov <dao-yan@live.ru>
2020-07-25 16:27:45 +0300

    Comp task-5

--- task-5.py
+++ task-5.py
@@ -4,3 +4,15 @@
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
 
+import random
+import functools
+
+with open(file="task-5.txt", mode="w+") as of:
+    print(" ".join(
+        [str(random.randint(-1000, 1000)) for i in range(1, random.randint(5, 10))]
+    ), file=of)
+    of.seek(0)
+    sum = 0
+    for ln in of:
+        sum += functools.reduce((lambda a, b: int(a) + int(b)), ln.split())
+print("Cумма чисел в файле {}".format(sum))
