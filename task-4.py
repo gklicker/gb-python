ce8e329971ecd8928d1ba4e8663ba1686f3d2276
Andrey Yushkov <dao-yan@live.ru>
2020-07-29 17:22:25 +0300

    Comp task-4

--- task-4.py
+++ task-4.py
@@ -8,3 +8,67 @@
 Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+
+
+class Car:
+    speed = 0
+    color = None
+    name = None
+    is_police = False
+
+    def __init__(self):
+        self.is_police = False
+
+    def go(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def turn(self, direction):
+        pass
+
+    def show_speed(self):
+        print("Speed {} kn/h".format(self.speed))
+
+class TownCar(Car):
+    def show_speed(self):
+        #  60
+        print("Speed {} kn/h{}".format(self.speed, "" if self.speed <= 60 else " превышение"))
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        #  40
+        print("Speed {} kn/h{}".format(self.speed, "" if self.speed <= 40 else " превышение"))
+
+class SportCar(Car):
+    pass
+
+class PoliceCar(Car):
+    def __init__(self):
+        self.is_police = True
+
+A = TownCar()
+A.speed = 60
+A.color = "red"
+A.name = "Name A"
+A.show_speed()
+
+B = WorkCar()
+B.speed = 60
+B.color = "green"
+B.name = "Name B"
+B.show_speed()
+
+C = SportCar()
+C.speed = 60
+C.color = "blue"
+C.name = "Name C"
+C.show_speed()
+
+D = PoliceCar()
+D.speed = 60
+D.color = "transparent"
+D.name = "Name D"
+D.show_speed()
